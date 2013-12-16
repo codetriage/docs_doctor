c4568d2c272b58eee95ae6809bd5444fdd47e4c6
schneems <richard.schneeman@gmail.com>
2013-12-16 15:10:22 -0600

    better validations

--- app/models/doc_method.rb
+++ app/models/doc_method.rb
@@ -2,9 +2,12 @@ class DocMethod < ActiveRecord::Base
   belongs_to :repo
   has_many   :doc_comments, dependent: :destroy
 
+  validates :file, :name, :path, presence: true
+
   include ActiveRecord::CounterCache
 
   def file
+    return nil if read_attribute(:file).blank?
     absolute, match, relative = read_attribute(:file).partition(/(\/|^)#{repo.name}\//)
     return relative
   end
